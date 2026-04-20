fcf67e62212bba5d827b70fb3308a464f410a180
Garv976 <sgarv87634@gmail.com>
2026-04-20 22:48:34 +0530

    Add queries for retrieving and sorting employees
    Added SQL queries to retrieve and sort employees by age.

--- 01_Data_Retrieval/05_order_by.sql
+++ 01_Data_Retrieval/05_order_by.sql
@@ -1,1 +1,8 @@
+use employees;
+select * from Employees;
+
+-- Sorting the data based on age
+select * from Employees order by age;
+select * from Employees order by age asc;
+select * from Employees order by age desc;
 
