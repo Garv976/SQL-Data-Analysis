b38402b66d4738a8419548ae2afccab29a9742e1
Garv976 <sgarv87634@gmail.com>
2026-04-21 23:29:26 +0530

    Add SQL queries for employee selection and case statement
    Added SQL queries to select employees and categorize by department.

--- 02_Cases_SQL/02_cases.sql
+++ 02_Cases_SQL/02_cases.sql
@@ -1,1 +1,10 @@
+use employees;
 
+select * from Employees;
+
+-- Basic case for sales team
+select name, 
+case department
+	when 'sales' then 'sales team'
+    else 'other'
+end as employee_name from Employees;
