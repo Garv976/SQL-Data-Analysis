0ab85f1af8a288fc2a7e238f07751eb614fcfafd
Garv976 <sgarv87634@gmail.com>
2026-04-22 23:46:12 +0530

    Create 01_create_table.sql

---
+++ 03_DDL_Commands/01_create_table.sql
@@ -0,0 +1,13 @@
+-- Creating first table
+drop table if exists employee;
+create table employee (employee_id INT, name VARCHAR(50), age INT, department VARCHAR(50));
+select * from employee;
+
+-- Table with constraints
+drop table if exists employee;
+create table employee (
+	employee_id INT, 
+    name VARCHAR(50) not null, 
+    age INT check (age >= 18), 
+    department VARCHAR(50));
+select * from employee;
